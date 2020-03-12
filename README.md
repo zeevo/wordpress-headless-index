8ae67d8dfea69e7444ce2b74c49b559114682a4f
zeevosec <oneill.shane.h@gmail.com>
2020-03-11 21:31:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# wordpress-headless-index
+
+This is just a useful index to drop into your headless themes
+
+![screenshot](./screenshot.png)
+
+## How-to
+
+Change the following lines of code to fit your admin location and site location
+
+```php
+<?php
+	$site_location='#';
+	$admin_location='/wp-admin'
+?>
+```
---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+	$site_location='#';
+	$admin_location='/wp-admin'
+?>
+
+<link
+  rel="stylesheet"
+  href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+  crossorigin="anonymous"
+/>
+
+<style>
+  .box {
+    margin-top: 100px;
+    text-align: center;
+  }
+  .card {
+    display: inline-block;
+  }
+</style>
+
+<div class="container box">
+  <div class="jumbotron">
+    <h1>Welcome</h1>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Admin</h5>
+      <p class="card-text">
+        Make new posts, configure site metadata, upload content
+      </p>
+      <a href="<?php echo $admin_location?>" class="btn btn-primary stretched-link">Proceed to Admin</a>
+    </div>
+  </div>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Site</h5>
+      <p class="card-text">
+        View changes, read posts and pages
+      </p>
+      <a href="<?php echo $site_location?>" class="btn btn-info stretched-link">Go Site</a>
+    </div>
+  </div>
+</div>
